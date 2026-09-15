0e735564f68257a79e9ed94869b4e649e8604d05
v0kcr <20231919@aloe.ulima.edu.pe>
2026-09-15 08:08:41 -0500

    Ventanas del Nav var unu

--- website/controllers.js
+++ website/controllers.js
@@ -8,6 +8,28 @@ export function home(req, res) {
   });
 }
 
+export function register(req, res) {
+  return res.render('website/register', {
+    title: 'Crear cuenta',
+  });
+}
+
+export function convocatoria(req, res) {
+  return res.render('website/convocatoria', {
+    title: 'Convocatorias',
+  });
+}
+export function empresa(req, res) {
+  return res.render('website/empresa', {
+    title: 'empresa',
+  });
+}
+export function comoFunciona(req, res) {
+  return res.render('website/comoFunciona', {
+    title: 'comoFunciona',
+  });
+}
+
 export function about(req, res) {
   return res.render('website/about', {
     title: 'Acerca de',
@@ -42,7 +64,25 @@ export function signIn(req, res) {
   });
 }
 
-export async function login(req, res) {
+export function login(req, res) {
+  let fondos = [
+    "/assets/img/login-bg.png",
+    "/assets/img/login-bg2.png",
+    "/assets/img/login-bg3.jpg"
+  ];
+  return res.render('website/login', {
+    title: 'Bienvenido',
+    background: fondos[Math.floor(Math.random() * 3)]
+  });
+}
+
+export function resetPassword(req, res) {
+  return res.render('website/reset-password', {
+    title: 'Recuperar Contraseña',
+  });
+}
+
+export async function login2(req, res) {
   const { user, password } = req.body;
   const validUser = process.env.DEFAULT_USER || 'admin';
   const validPassword = process.env.DEFAULT_PASSWORD || '123';
--- website/routes.js
+++ website/routes.js
@@ -7,6 +7,12 @@ import { redirectIfAuthenticated, requireAuth } from '../configs/middlewares.js'
 const router = Router();
 
 router.get('/', controller.home);
+router.get('/login', controller.login);
+router.get('/register', controller.register);
+router.get('/convocatoria', controller.convocatoria);
+router.get('/empresa', controller.empresa);
+router.get('/como-funciona', controller.comoFunciona);
+router.get('/reset-password', controller.resetPassword);
 router.get('/about', controller.about);
 router.get('/contact', controller.contact);
 router.get('/players', controller.players);
